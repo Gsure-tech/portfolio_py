b94f505b532078de279cbfbf7e801700dccdab9c
gsure-tech <Geeescode@>
2025-01-08 21:30:56 +0100

    Day 22: Add content on main

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+content = """
+Hi, I am Abdulganiyu! I am a Software Engineer, teacher and the founder of GsureTech. 
+I graduated in 2019 with a Bachelor Degree in Computer Science from Bayero University Kano in Nigeria. I have worked with companies from various states, 
+such as Maritime Academy as a System Analyst, Hardcore Biometrics as a Software Engineer
+"""
+
+
