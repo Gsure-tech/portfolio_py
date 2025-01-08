0cb7994b0cdef25ddcfb2ba457b7a7cc228a3a5c
gsure-tech <Geeescode@>
2025-01-08 22:02:53 +0100

    Day 22: Add content on main

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import streamlit as st
+import pandas
+
+col1, empty_col, col2 = st.columns([])
 
 content = """
 Hi, I am Abdulganiyu! I am a Software Engineer, teacher and the founder of GsureTech. 
 I graduated in 2019 with a Bachelor Degree in Computer Science from Bayero University Kano in Nigeria. I have worked with companies from various states, 
 such as Maritime Academy as a System Analyst, Hardcore Biometrics as a Software Engineer
 """
+    st.info(content)
+
+
+st
+
 
 
