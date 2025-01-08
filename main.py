9e04923b6d1d8a1bb042bf65e807ab7fb477204e
gsure-tech <Geeescode@>
2025-01-08 22:10:39 +0100

    Day 22: Add title

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import streamlit as st
 import pandas
 
-col1, empty_col, col2 = st.columns([])
+col1, empty_col, col2 = st.columns([1,1.5,1])
 
-content = """
-Hi, I am Abdulganiyu! I am a Software Engineer, teacher and the founder of GsureTech. 
-I graduated in 2019 with a Bachelor Degree in Computer Science from Bayero University Kano in Nigeria. I have worked with companies from various states, 
-such as Maritime Academy as a System Analyst, Hardcore Biometrics as a Software Engineer
-"""
+with col1:
+    st.image("images/gee_profile.jpg")
+
+with col2:
+    st.title("Abdulganiyu Abubakar")
+    content = """
+    Hi, I am Abdulganiyu! I am a Software Engineer, teacher and the founder of GsureTech. 
+    I graduated in 2019 with a Bachelor Degree in Computer Science from Bayero University Kano in Nigeria. I have worked with companies from various states, 
+    such as Maritime Academy as a System Analyst, Hardcore Biometrics as a Software Engineer
+    """
     st.info(content)
 
 
-st
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+
+st.write(content2)
+
+col3, col4 = st.columns(2)
 
 
 
