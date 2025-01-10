0f463eb2ce5acc58a78a7e8fc35163ffe9f33161
gsure-tech <Geeescode@>
2025-01-10 23:41:49 +0100

    add server login

--- send_email.py
+++ send_email.py
@@ -7,3 +7,7 @@ username = "abdulganiyu9023@gmail.com"
 password="pgno dkrh usyp rjyo"
 
 context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(host, port, context) as server:
+    server.login(username, password)
+
