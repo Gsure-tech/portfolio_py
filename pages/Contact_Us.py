27a0c6965a85128c7951ba2b91f6a121319500a0
gsure-tech <Geeescode@>
2025-01-11 00:00:53 +0100

    implement email script to form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,12 +1,13 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
     message = st.text_area("Your message")
+    message = message + "\n" + user_email
     button = st.form_submit_button("Submit")
 
 if button:
-    print(button)
-    print("I was pressed")
+   send_email(message)
